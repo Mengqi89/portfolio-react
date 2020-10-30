e65d5aedc870b144474a1a87b85440f0554b7535
Mengqi Wang <wmq516@gmail.com>
2020-10-30 15:15:52 -0600

    completed routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import ContactPage from './pages/ContactPage'
+import AboutPage from './pages/AboutPage'
 import './App.css'
+
 
 class App extends React.Component {
   state = {
@@ -24,9 +34,25 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        {this.state.title}
-      </div>
+        <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="sm">
+              <Navbar.Brand>Mengqi Wang</Navbar.Brand>
+              <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+                <Navbar.Collapse id="navbar-toggle">
+                  <Nav className="ml-auto">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+                  </Nav>
+                </Navbar.Collapse>
+          </Navbar>
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />}></Route>
+          <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />}></Route>
+          <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} /> }></Route>
+          <Footer />
+          </Container>
+        </Router>
     )
   }
   
