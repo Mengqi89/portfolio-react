28cb9d2a0387b574ac3774d3abdb4e93ba0a7219
Mengqi Wang <wmq516@gmail.com>
2020-11-01 18:31:53 -0700

    adjusted title font size and icon sizes

--- src/components/Internships.js
+++ src/components/Internships.js
@@ -12,6 +12,7 @@ function Tech(props) {
             <Row className="justify-content-around py-5">
                 <Col className="text-center col-internship" xs={{ span: 12, offset: 0 }} md={{ span: 6, offset: 0 }}><img className="internship-pic" src={frame} alt="frame logo"></img></Col>
                 <Col className="text-left col-internship" xs={{ span: 12, offset: 0 }} md={{ span: 6, offset: 0 }}>
+                    <p><h3>Frame</h3></p>
                     <p>I have joined Frame, a solar proposal start-up, as an intern, since late September 2020. I have worked on customer profile and messaging functions.</p>
                     <p>Despite being completely new to the tech stack, I have been able to meet weekly deliverable goals and build new features from the beginning.</p>
                     <p className="font-weight-bold">Tech Stack: Vue.js, Node.js, Firebase, BootstrapVue, Sass</p>
@@ -19,6 +20,7 @@ function Tech(props) {
             </Row>
             <Row className="justify-content-around py-5">
                 <Col className="text-left col-internship" xs={{ span: 12, offset: 0, order: 12 }} md={{ span: 6, offset: 0, order: 1 }}>
+                    <p><h3>Just Utah Coders</h3></p>
                     <p>
                         From March to August 2020, I joined <a href="https://github.com/JustUtahCoders/" target="_blank" rel="noopenner noreferrer">Just Utah Coders</a> as an intern to help build <a href="https://utahexpungements.org" target="_blank" rel="noopenner noreferrer">utahexpungements.org</a>. I jumped on board with minimal supervision and worked with Salt Lake County Expungement Coordinator to improve the forms tool. I was also able to identify and fix inconsistent datakeys to improve cross-form data population.
                     </p>
